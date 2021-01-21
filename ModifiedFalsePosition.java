97038782ea6a99c29bd2c61d2804fc49962718cb
Austin Back <tartsofficial@gmail.com>
2021-01-21 11:41:51 -0500

    Fixed mid function

--- ModifiedFalsePosition.java
+++ ModifiedFalsePosition.java
@@ -4,8 +4,8 @@ public class ModifiedFalsePosition {
 		//get intervals
 		double[] arr = signSwitch(0,1);
 		//run bisection and print results
-		for(int i = 0; i < 5;i++) {
-			System.out.println("iteration " + i + " = " + FalsePos(arr[0],arr[1],i));
+		for(int i = 1; i < 6;i++) {
+			System.out.println("iteration " + i + " = " + ModFalsePos(arr[0],arr[1],i-1));
 		}
 		
 	}
@@ -29,22 +29,22 @@ public class ModifiedFalsePosition {
 	}
 	
 	//false position algo
-	private static double FalsePos(double lower,double upper,int iterations) {
+	private static double ModFalsePos(double lower,double upper,int iterations) {
 		//Initialize variables
 		double left,right;
 		left = lower;
 		right = upper;
 		//get initial mid point
-		double mid = mid(left,right);
+		double mid = mid(left,f(left),right,f(right));
 		//run for iteration amount of times
 		for(int i = 0; i < iterations;i++) {
 			//initial if statements check for how to change points, final else checks if root has already been found
 			if(f(left)*f(mid) > 0) {
 				left = mid;
-				mid = mid(mid,right/2);
+				mid = mid(mid,f(mid),right,f(right)/2);
 			}else if(f(left)*f(mid) < 0) {
 				right = mid;
-				mid = mid(left/2,mid);
+				mid = mid(left,f(left)/2,mid,f(mid));
 			}else{
 				return mid;
 			}
@@ -54,11 +54,11 @@ public class ModifiedFalsePosition {
 	}
 	
 	//get equation of line connecting the two points and return when f(x)=0
-	private static double mid(double left,double right) {
+	private static double mid(double left,double fleft,double right, double fright) {
 		//get slope
-		double m = (f(right)-f(left))/(right-left);
+		double m = (fright-fleft)/(right-left);
 		//get y-intercept
-		double b = f(right)-m*right;
+		double b = fleft-m*left;
 		//return x for when y=0
 		return (-b)/m;
 	}
